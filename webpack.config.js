aca02184361d3935a11d6f2a1d4c028b93fcaf9c
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-04-26 09:34:01 +0100

    Add Linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -21,27 +20,27 @@ module.exports = {
     publicPath: '/',
   },
   module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-     {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-       type: 'asset/resource',
-     },
-     {
-       test: /\.(woff|woff2|eot|ttf|otf)$/i,
-       type: 'asset/resource',
-     },
-     {
-       test: /\.(csv|tsv)$/i,
-       use: ['csv-loader'],
-     },
-     {
-       test: /\.xml$/i,
-       use: ['xml-loader'],
-     },
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
     ],
   },
 };
