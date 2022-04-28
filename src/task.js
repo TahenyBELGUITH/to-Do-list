d21611ee4e29ca06987d61795dda729897d340f4
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-04-28 07:53:19 +0100

    Refactor code

--- src/task.js
+++
@@ -1,9 +0,0 @@
-import { tasksArray } from './variables.js'; //eslint-disable-line
-
-export default class Task {
-  constructor(userTask) {
-    this.id = tasksArray.length + 1;
-    this.task = userTask.value;
-    this.status = false;
-  }
-}
